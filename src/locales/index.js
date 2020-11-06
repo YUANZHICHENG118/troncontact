f3ebdcddc0a2bc5422daf354ad7c7808cec27caf
jiay <jia_any@163.com>
2020-11-06 16:19:52 +0800

    lang

--- src/locales/index.js
+++ src/locales/index.js
@@ -1,17 +1,15 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 // default language
-import ZH from './lang/zh'
-import EN from './lang/en'
-import KO from './lang/ko'
+import zhCN from './lang/zh'
 Vue.use(VueI18n)
 
 export const defaultLang = 'zh'
 
 const messages = {
-  'zh': ZH,
-  'en': EN,
-  'ko': KO,
+  'zh': {
+    ...zhCN
+  }
 }
 
 const i18n = new VueI18n({
@@ -19,5 +17,44 @@ const i18n = new VueI18n({
   fallbackLocale: defaultLang,
   messages
 })
+
 export default i18n
 
+const loadedLanguages = [defaultLang]
+
+const _lan = localStorage.getItem('language')
+// 从缓存設置中加载当前语言
+if (_lan !== null && defaultLang !== _lan) {
+  loadLanguageAsync(_lan)
+}
+
+function setI18nLanguage (lang) {
+  i18n.locale = lang
+  document.querySelector('html').setAttribute('lang', lang)
+  return lang
+}
+
+/**
+ * i18n Render
+ * @param key
+ * @returns rendered string
+ */
+export function i18nRender (key) {
+  return i18n.t(key)
+}
+
+export function loadLanguageAsync (lang = defaultLang) {
+  return new Promise(resolve => {
+    if (i18n.locale !== lang) {
+      if (!loadedLanguages.includes(lang)) {
+        return import(/* webpackChunkName: "lang-[request]" */ `./lang/${lang}`).then(msg => {
+          i18n.setLocaleMessage(lang, msg.default)
+          loadedLanguages.push(lang)
+          return setI18nLanguage(lang)
+        })
+      }
+      return resolve(setI18nLanguage(lang))
+    }
+    return resolve(lang)
+  })
+}
