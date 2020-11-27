4120288aed0d9d059e113d3a222ac172bb056e9c
yuanzhicheng <313640087@qq.com>
2020-11-27 17:09:16 +0800

    init

--- src/layout/components/Sidebar/ShareOptions.vue
+++ src/layout/components/Sidebar/ShareOptions.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="share-options" >
-    <a href="https://www.facebook.com/contract.tron.3" target="_blank"><img src="@/assets/facebook.png" alt=""></a>
-    <a href="https://twitter.com/troncontract1" target="_blank"><img src="@/assets/twitter.png" alt=""></a>
-    <a href="https://t.me/troncontractcc" target="_blank"><img src="@/assets/telegram.png" alt=""></a>
-    <a href="https://t.me/troncontractru" target="_blank"><img src="@/assets/telegram.png" alt=""></a>
-    <a href="https://www.instagram.com/troncontractcc" target="_blank"><img src="@/assets/instagram.svg" alt=""></a>
+    <a href="https://www.facebook.com/11 target="_blank"><img src="@/assets/facebook.png" alt=""></a>
+    <a href="https://twitter.com/11" target="_blank"><img src="@/assets/twitter.png" alt=""></a>
+    <a href="https://t.me/11" target="_blank"><img src="@/assets/telegram.png" alt=""></a>
+    <a href="https://t.me/11" target="_blank"><img src="@/assets/telegram.png" alt=""></a>
+    <a href="https://www.instagram.com/11" target="_blank"><img src="@/assets/instagram.svg" alt=""></a>
   </div>
 
 </template>
--- src/layout/components/Sidebar/ShareTeam.vue
+++ src/layout/components/Sidebar/ShareTeam.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="share-options" >
-    <a href="https://t.me/troncontractcc" target="_blank"><img src="@/assets/telegram.svg" alt=""><img  src="@/assets/eng.svg" alt=""></a>
-    <a href="https://t.me/troncontractru" target="_blank"><img src="@/assets/telegram.svg" alt=""><img src="@/assets/e.svg" alt=""></a>
-    <a href="https://t.me/troncontractcn" target="_blank"><img src="@/assets/telegram.svg" alt=""><img  src="@/assets/cn.svg" alt=""></a>
+    <a href="https://t.me/111" target="_blank"><img src="@/assets/telegram.svg" alt=""><img  src="@/assets/eng.svg" alt=""></a>
+    <a href="https://t.me/111" target="_blank"><img src="@/assets/telegram.svg" alt=""><img src="@/assets/e.svg" alt=""></a>
+    <a href="https://t.me/111" target="_blank"><img src="@/assets/telegram.svg" alt=""><img  src="@/assets/cn.svg" alt=""></a>
   </div>
 
 </template>
